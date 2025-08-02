e7c784a73b65e5866b2657cb5e4e724210b4bdc7
nekosich <mixail_sova@mail.ru>
2025-08-02 14:52:48 +0800

    Update commands.js

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,1 +1,46 @@
+import { ls, cd, cat, getCurrentPath } from './fs.js';
+
+const commands = {
+  help: () => `
+Available commands:
+- help
+- clear
+- ls
+- cd [dir]
+- cat [file]
+- shutdown
+`,
+
+  clear: () => {
+    const output = document.getElementById('output');
+    output.innerHTML = '';
+    return '';
+  },
+
+  ls: () => ls(),
+
+  cd: (arg) => {
+    try {
+      cd(arg || '/');
+      return `Current directory: ${getCurrentPath()}`;
+    } catch (err) {
+      return err.message;
+    }
+  },
+
+  cat: (arg) => {
+    try {
+      return cat(arg);
+    } catch (err) {
+      return err.message;
+    }
+  },
+
+  shutdown: () => {
+    window.location.href = "index.html";
+    return "System shutting down...";
+  }
+};
+
+export default commands;
 
